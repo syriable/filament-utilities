96287feaa23cee2d2c3e149aa9aa867f343e098d
syriable <info@syriable.com>
2026-06-07 14:50:41 +0200

    feat: add helpers functions

---
+++ src/helpers.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+
+use function Filament\Support\discover_app_classes;
+
+if (! function_exists('discover_classes')) {
+    /**
+     * @return array<class-string>
+     */
+    function discover_classes(?string $parentClass): array
+    {
+        return discover_app_classes($parentClass);
+    }
+}
+
+if (! function_exists('discover_package_classes')) {
+    /**
+     * @param  class-string<Model>|null  $parentClass
+     * @return list<class-string<Model>>
+     */
+    function discover_package_classes(?string $parentClass = null, string $packageName = 'modules'): array
+    {
+        $classLoader = require base_path('vendor/autoload.php');
+
+        /** @var array<class-string<Model>, string> $classMap */
+        $classMap = $classLoader->getClassMap();
+
+        $classes = [];
+
+        foreach ($classMap as $class => $file) {
+            if (blank($packageName) || blank($parentClass)) {
+                continue;
+            }
+
+            if (! (str($file)->contains(DIRECTORY_SEPARATOR . $packageName . DIRECTORY_SEPARATOR) ||
+                str($file)->contains('/' . $packageName . '/') ||
+                str($file)->contains('\\' . $packageName . '\\'))) {
+                continue;
+            }
+
+            if (! is_subclass_of($class, $parentClass)) {
+                continue;
+            }
+
+            $classes[] = $class;
+        }
+
+        return $classes;
+    }
+}
